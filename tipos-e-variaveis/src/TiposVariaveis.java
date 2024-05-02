97540ce77616f60eca9a6af69ad748270d1b191e
ViniRivaldar <vinicius-rivaldar@outlook.com>
2024-05-02 06:02:36 -0300

    adcionando os tipos

--- tipos-e-variaveis/src/TiposVariaveis.java
+++ tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,3 +1,13 @@
 public class TiposVariaveis {
+
+    public static void main(String[] args) throws Exception {
+        byte numero1 = 100;
+        short  numero2=numero1;
+        numero1 = (byte) numero2;
+        System.out.println(numero1);
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+    }
     
 }
